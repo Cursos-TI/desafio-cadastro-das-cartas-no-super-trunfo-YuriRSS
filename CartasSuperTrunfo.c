f9723db54475700e810a9f7f98e5f76a8d85bfee
YuriRSS <mikeferreira732@gmail.com>
2025-04-17 04:46:29 +0000

    Meu trabalho SuperTrunfo (Yuri)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,100 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    //Carta 1
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado1;
+    char codigocarta1 [20];
+    char cidade1 [20];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosturisticos1;
 
-    return 0;
+     //Carta 2
+
+     char estado2;
+     char codigocarta2 [20];
+     char cidade2 [20];
+     int populacao2;
+     float area2;
+     float pib2;
+     int pontosturisticos2;
+
+    //Cadastramento da primeira carta:
+
+    printf("Cadastre a sua primeira carta. \n");
+
+    printf("Digite uma letra que represente um Estado: \n");
+    scanf("%c", &estado1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", codigocarta1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite o nome de uma cidade: \n");
+    scanf("%s", cidade1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", &populacao1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite a área (km) da cidade: \n");
+    scanf("%f", &area1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite a quantidade de pontos turísticos da cidade: \n");
+    scanf("%d", &pontosturisticos1);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Carta cadastrada com sucesso! \n");
+
+    printf(" - Estado: %c\n - Código: %s\n - Cidade: %s\n - População: %d\n - Área: %.2f\n - PIB: %.2f\n - Pontos Turísticos: %d\n", estado1, codigocarta1, cidade1, populacao1, area1, pib1, pontosturisticos1);
+
+    //Cadastramento da segunda carta:
+
+    printf("Agora cadastre a segunda carta. \n");
+
+    printf("Digite uma letra que represente um Estado: \n");
+    scanf(" %c", &estado2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", codigocarta2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite o nome de uma cidade: \n");
+    scanf("%s", cidade2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", &populacao2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite a área (km) da cidade: \n");
+    scanf("%f", &area2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Digite a quantidade de pontos turísticos da cidade: \n");
+    scanf("%d", &pontosturisticos2);
+    printf("Dado salvo com sucesso! \n");
+
+    printf("Carta cadastrada com sucesso! \n");
+
+    printf(" - Estado: %c\n - Código: %s\n - Cidade: %s\n - População: %d\n - Área: %.2f\n - PIB: %.2f\n - Pontos Turísticos: %d\n", estado2, codigocarta2, cidade2, populacao2, area2, pib2, pontosturisticos2);
+
+    printf("As duas cartas foram armazenadas no sistema! \n");
+
+    
+    
 }
